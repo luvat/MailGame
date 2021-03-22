babbc441f7a9a282bf73b5b70c0ea082f1125b0b
Elanor <elanor.berg@elev.ga.lbs.se>
2021-03-22 22:07:52 +0100

    Början av dialog
    började göra dialog, ska göra mer  imorgon för inte i närheten av klart

--- Assets/Code/Movement.cs
+++ Assets/Code/Movement.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
 
     public int movementSpeed = 5; //Variabel för hur fort Mailman ska kunna gå, kan också ändras genom unity då-Elanor
 
+    public GameObject text; //Referens till text gameobject - Elanor
+    public Text dialog; //Referens till UI:n vad som står. -Elanor
 
     public Rigidbody2D body; // Referens till RigedBody2D i player-Elanor
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>(); //Den hämtar Rigedbody2D och sätter in det i body-Elanor
@@ -38,5 +42,24 @@ public class Movement : MonoBehaviour
         {
             body.velocity = (new Vector3(body.velocity.x, 0, 0)); //Gör så att om man släpper på S kommer mailman sluta röra sig uppåt-Elanor
         }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {  if (Input.GetKeyDown(KeyCode.T)) //Gör så att om man är innanför collisonen och trycker på T så kommer texten komma up-Elanor
+        {
+            if (collision.transform.tag == "talk") //Om man är inaför collisonen?-Elanor
+            {
+                text.SetActive(true); //Så kommer UI:n synas (alltså det jag kommer ha skrivt kommer synas)-Elanor 
+                dialog.text = "heej";
+            }
+        }
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+         if (collision.transform.tag == "talk") //Om man är inte är inaför collisonen?-Elanor
+         {
+              text.SetActive(false); //Kommer UI:n inte att synas-Elanor
+         }
+           
     }
 }
