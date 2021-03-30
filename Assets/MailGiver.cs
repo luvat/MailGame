4a4eb8928dffeeed61635f5964d1f4920ba8773f
luvat <79506363+luvat@users.noreply.github.com>
2021-03-30 09:21:31 +0200

    Drag and drop

---
+++ Assets/MailGiver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MailGiver : MonoBehaviour
+{
+   //detects the dragging of the object
+    private bool isDragging;
+
+    public void OnMouseDown() //När du klickar ned med musen så börjar den dra.
+    {
+        isDragging=true;
+    }
+
+    private void OnMouseUp() //När du släpper musen slutar den dra.
+    {
+        isDragging = false;
+    }
+    private void Update()
+    {
+        if (isDragging) //Om du klickar ned musen så börjar brevet röra sig.
+        {
+            Vector2 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mouseposition);
+        }
+    }
+
+   
+
+}
