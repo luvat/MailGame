a90fb321959be79559d7e72c4ad40c750bd286cb
Iron-Lotus <victor.jonsson@elev.ga.lbs.se>
2021-03-30 10:37:46 +0200

    Meny
    Typ klar

--- Assets/PlayGame.cs
+++ Assets/PlayGame.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame ()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+        
+    
 }
